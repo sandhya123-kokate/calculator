acf4a1b689ecfc3af05c780b766e916091b623bd
sandhyakokate <sandhyarkokate@coep.sveri.ac.in>
2024-07-10 00:18:17 +0530

    change

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.getElementById('mathmatical').addEventListener('click', function() {
 
     console.log("click");
-    window.location.href = 'Mathamatical/index.html';
+    window.location.href = 'Home/Mathamatical/index.html';
 });
 
 document.getElementById('age').addEventListener('click', function() {
 
   
-    window.location.href = 'AgeCal/index.html';
+    window.location.href = 'Home/AgeCal/index.html';
 });
 document.getElementById('currency').addEventListener('click', function() {
 
   
-    window.location.href = 'Currency/index.html';
+    window.location.href = 'Home/Currency/index.html';
 });
 
 document.getElementById('unit').addEventListener('click', function() {
 
   
-    window.location.href = 'Unit/index.html';
+    window.location.href = 'Home/Unit/index.html';
 });
