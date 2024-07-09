6ee56ce647a28790d47e20a90be1aec81ad2e034
sandhyakokate <sandhyarkokate@coep.sveri.ac.in>
2024-07-10 00:05:00 +0530

    first commit

---
+++ Home/script.js
@@ -0,0 +1,22 @@
+document.getElementById('mathmatical').addEventListener('click', function() {
+
+    console.log("click");
+    window.location.href = 'Mathamatical/index.html';
+});
+
+document.getElementById('age').addEventListener('click', function() {
+
+  
+    window.location.href = 'AgeCal/index.html';
+});
+document.getElementById('currency').addEventListener('click', function() {
+
+  
+    window.location.href = 'Currency/index.html';
+});
+
+document.getElementById('unit').addEventListener('click', function() {
+
+  
+    window.location.href = 'Unit/index.html';
+});
